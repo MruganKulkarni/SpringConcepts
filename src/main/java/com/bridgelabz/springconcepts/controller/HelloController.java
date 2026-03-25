eeefc8f55550fa8372e1272740e12e406c69a943
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-03-25 12:03:51 +0530

    [Mrugan Kulkarni]: UC2 Added SLF4J logging to controller

--- src/main/java/com/bridgelabz/springconcepts/controller/HelloController.java
+++ src/main/java/com/bridgelabz/springconcepts/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.bridgelabz.springconcepts.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
     public String hello() {
+
+        logger.info("Received request for Hello endpoint");
+        logger.debug("Debugging HelloController");
+
         return "Welcome to Spring Concepts App";
     }
 
